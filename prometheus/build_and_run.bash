3a5fa2b8dcbe0a1b7c68cc4336a8ea7d2cac5d71
Igor <iroztropinski@gmail.com>
2022-11-26 16:03:32 +0100

    Increase prometheus data retention period

--- prometheus/build_and_run.bash
+++ prometheus/build_and_run.bash
@@ -7,6 +7,9 @@ docker build . -t prometheus-exp
 docker volume create prometheus-exp-volume
 
 #9090 port we have
-docker run --log-driver=fluentd --log-opt tag="docker.{{.ID}}" --log-opt labels="instance-id" \
+#--log-driver=fluentd --log-opt tag="docker.{{.ID}}" --log-opt labels="instance-id" \
+docker run \
   --label "instance-id=prometheus-0" \
-  --network host -v "prometheus-exp-volume:/prometheus" --name prometheus-exp prometheus-exp
+  --network host -v "prometheus-exp-volume:/prometheus" \
+  --name prometheus-exp prometheus-exp  \
+  --storage.tsdb.retention.time=1m  --config.file=/etc/prometheus/prometheus.yml
