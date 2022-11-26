156a0ba4661e7f159fca82ce9634a85b40d46022
Igor <iroztropinski@gmail.com>
2022-11-26 16:56:31 +0100

    grafana in repo config

--- grafana/build_and_run_grafana.bash
+++ grafana/build_and_run_grafana.bash
@@ -3,6 +3,7 @@ docker rm grafana-exp
 
 docker build . -t grafana-exp
 
-docker volume create grafana-exp-volume
+# docker volume create grafana-exp-volume
+# docker run --network host -v "grafana-exp-volume:/var/lib/grafana" --name grafana-exp grafana-exp
 
-docker run --network host -v "grafana-exp-volume:/var/lib/grafana" --name grafana-exp grafana-exp
+docker run --network host --name grafana-exp grafana-exp
