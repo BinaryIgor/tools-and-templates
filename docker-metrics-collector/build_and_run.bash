9456ba0ae93c91dc1dea0aa9025cf173d4902a36
Igor <iroztropinski@gmail.com>
2022-11-27 16:42:03 +0100

    Fix gauges references

--- docker-metrics-collector/build_and_run.bash
+++ docker-metrics-collector/build_and_run.bash
@@ -4,4 +4,7 @@ docker build . -t docker-metrics-collector
 
 docker rm docker-metrics-collector
 
-docker run -v /var/run/docker.sock:/var/run/docker.sock --name docker-metrics-collector docker-metrics-collector
+docker run -v /var/run/docker.sock:/var/run/docker.sock \
+  --network host \
+  -e "METRICS_TARGET_URL=http://localhost:10101/metrics" \
+  --name docker-metrics-collector docker-metrics-collector
--- system-monitor/src/main/java/com/igor101/system/monitor/metrics/infrastructure/MetricsService.java
+++ system-monitor/src/main/java/com/igor101/system/monitor/metrics/infrastructure/MetricsService.java
@@ -3,8 +3,15 @@ package com.igor101.system.monitor.metrics.infrastructure;
 import com.igor101.system.monitor.common.Metrics;
 import com.igor101.system.monitor.metrics.application.ContainersMetrics;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tag;
 import io.micrometer.core.instrument.Tags;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.ToDoubleFunction;
+import java.util.stream.Collectors;
 
 @Service
 public class MetricsService {
@@ -14,6 +21,7 @@ public class MetricsService {
     private static final String APPLICATION_USED_MEMORY = Metrics.fullName("application_used_memory_bytes");
     private static final String APPLICATION_MAX_MEMORY = Metrics.fullName("application_max_memory_bytes");
     private static final String APPLICATION_CPU_USAGE = Metrics.fullName("application_cpu_usage_percent");
+    private final Map<String, AtomicReference<Double>> metricsReferences = new ConcurrentHashMap<>();
     private final MeterRegistry meterRegistry;
 
     public MetricsService(MeterRegistry meterRegistry) {
@@ -27,13 +35,27 @@ public class MetricsService {
 
             var secondsTimestamp = m.timestamp() / 1000.0;
 
-            meterRegistry.gauge(COLLECTOR_UP_TIMESTAMP, Tags.of(Metrics.SOURCE_LABEL, containersMetrics.source()),
+            setMetricValue(COLLECTOR_UP_TIMESTAMP, Tags.of(Metrics.SOURCE_LABEL, containersMetrics.source()),
                     secondsTimestamp);
 
-            meterRegistry.gauge(APPLICATION_UP_TIMESTAMP, tags, secondsTimestamp);
-            meterRegistry.gauge(APPLICATION_USED_MEMORY, tags, m.usedMemory());
-            meterRegistry.gauge(APPLICATION_MAX_MEMORY, tags, m.maxMemory());
-            meterRegistry.gauge(APPLICATION_CPU_USAGE, tags, m.cpuUsage());
+            setMetricValue(APPLICATION_UP_TIMESTAMP, tags, secondsTimestamp);
+            setMetricValue(APPLICATION_USED_MEMORY, tags, m.usedMemory());
+            setMetricValue(APPLICATION_MAX_MEMORY, tags, m.maxMemory());
+            setMetricValue(APPLICATION_CPU_USAGE, tags, m.cpuUsage());
         }
+    }
+
+    private void setMetricValue(String metric, Tags tags, double value) {
+        var previousReference = metricsReferences.computeIfAbsent(metricReferenceId(metric, tags),
+                k -> new AtomicReference<>());
+        previousReference.set(value);
+
+        meterRegistry.gauge(metric, tags, previousReference, AtomicReference::get);
+    }
+
+    private String metricReferenceId(String metric, Tags tags) {
+        return metric + "_" + tags.stream()
+                .map(Tag::getValue)
+                .collect(Collectors.joining());
     }
 }
