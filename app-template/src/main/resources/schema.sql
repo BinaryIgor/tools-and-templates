96ba72bc59d970fc6dd2f2d43086f975728182a5
Igor <iroztropinski@gmail.com>
2022-12-18 19:50:19 +0100

    Set up project schema

--- app-template/src/main/resources/schema.sql
+++ app-template/src/main/resources/schema.sql
@@ -21,3 +21,36 @@ CREATE TABLE activation_token (
     PRIMARY KEY(id, type)
 );
 CREATE INDEX activation_token_expires_at on activation_token(expires_at);
+
+
+CREATE SCHEMA project;
+
+SET SEARCH_PATH=project;
+
+CREATE TABLE project (
+    id UUID PRIMARY KEY,
+    owner_id UUID NOT NULL REFERENCES "user".user(id) ON DELETE CASCADE,
+    name TEXT NOT NULL UNIQUE,
+    version BIGSERIAL NOT NULL
+);
+
+CREATE TABLE project_user (
+    project_id UUID NOT NULL REFERENCES project(id) ON DELETE CASCADE,
+    user_id UUID NOT NULL REFERENCES "user".user(id) ON DELETE CASCADE,
+    PRIMARY KEY(project_id, user_id)
+);
+
+CREATE TABLE task (
+    id UUID PRIMARY KEY,
+    creator_id UUID NOT NULL REFERENCES "user".user(id) ON DELETE CASCADE,
+    assignee_id UUID NOT NULL REFERENCES "user".user(id) ON DELETE CASCADE,
+    name TEXT NOT NULL UNIQUE,
+    description TEXT,
+    version BIGSERIAL NOT NULL
+);
+
+CREATE TABLE project_task (
+    project_id UUID NOT NULL REFERENCES project(id) ON DELETE CASCADE,
+    task_id UUID NOT NULL REFERENCES task(id) ON DELETE CASCADE,
+    PRIMARY KEY(project_id, task_id)
+);
