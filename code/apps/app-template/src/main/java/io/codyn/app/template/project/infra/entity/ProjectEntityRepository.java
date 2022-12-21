5dfd5822ab8de5bb7e7e605f0f55ee8c0d4bbb4a
Igor <iroztropinski@gmail.com>
2022-12-21 21:06:26 +0100

    Fixing multi module queries

--- code/apps/app-template/src/main/java/io/codyn/app/template/project/infra/entity/ProjectEntityRepository.java
+++ code/apps/app-template/src/main/java/io/codyn/app/template/project/infra/entity/ProjectEntityRepository.java
@@ -2,6 +2,7 @@ package io.codyn.app.template.project.infra.entity;
 
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,8 +11,8 @@ import java.util.UUID;
 public interface ProjectEntityRepository extends CrudRepository<ProjectEntity, UUID> {
 
     @Query("SELECT owner_id FROM project.project WHERE id = :id")
-    Optional<UUID> findOwnerById(UUID id);
+    Optional<UUID> findOwnerById(@Param("id") UUID id);
 
     @Query("SELECT user_id FROM project.project_user WHERE project_id = :id")
-    List<UUID> findProjectUsersById(UUID id);
+    List<UUID> findProjectUsersById(@Param("id") UUID id);
 }
